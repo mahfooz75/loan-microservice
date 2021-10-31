91cb568bb902c2a8af4917048fb7edfb12b1f931
Mahfooz Ahmad <mahfooz.lakhani@gmail.com>
2021-10-31 12:52:47 +0530

    Loan Microservice

---
+++ src/main/java/com/mahfooz/loans/repository/LoansRepository.java
@@ -0,0 +1,13 @@
+package com.mahfooz.loans.repository;
+
+import java.util.List;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import com.mahfooz.loans.model.Loans;
+
+@Repository
+public interface LoansRepository extends CrudRepository<Loans, Long> {
+
+  List<Loans> findByCustomerIdOrderByStartDtDesc(int customerId);
+
+}
